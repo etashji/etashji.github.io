fd1162c06529203fff66b1fcaf9303c83201b933
Eric Tashji <99293073+etashji@users.noreply.github.com>
2024-10-21 15:18:58 -0400

    Origin

---
+++ JS/portfolio.js
@@ -0,0 +1,44 @@
+// These functions open and close the contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// This code will create close the contact from when the user clicks off of it
+// The first step is to add an event listener for any clicks on the website
+document.addEventListener("click", function(event){
+    // Here we state that if the click happens on the cancel button OR anywhere that is not the contact form And the click does not happen on any element with the contact class then call the closeForm() function
+    if (event.target.matches(".cancel") || !event.target.closest(".form-popup") && !event.target.closest(".Pop_Up_Button") && !event.target.closest(".contact")){
+        closeForm()
+    }
+}, false )
